56c1cfe54f1dfe7c5faebd421251e57d94da2b72
Askari <blgntobi@gmail.com>
2020-03-06 08:02:33 +0100

    done with my event cearting app

---
+++ nuxt.config.js
@@ -0,0 +1,63 @@
+import EventService from './services/EventService.js'
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+ generate: {
+  routes: () => {
+    return EventService.getEvents().then(response => {
+      return response.data.map(event => {
+        return '/event/' + event.id
+      })
+    })
+  }
+},
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  // loading: { color: '#fff' },
+  loading: { color: '#39b982' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <p> About me </p>
+</template>
